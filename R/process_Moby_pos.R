c367579feef3b01541323effb0a84391b5a4b77c
Matt Cook <cookm346@myumanitoba.ca>
2019-12-04 13:20:04 -0600

    add Moby pos processing and data

---
+++ R/process_Moby_pos.R
@@ -0,0 +1,27 @@
+pos <- readLines("http://www.gutenberg.org/files/3203/files/mobypos.txt")
+pos2 <- strsplit(pos,"[\\\\]|[^[:print:]]",fixed=FALSE)
+
+first <- unlist(lapply(pos2, function(x)paste0(x[1:(length(x)-1)], collapse = "")))
+last <- unlist(lapply(pos2, function(x)x[length(x)]))
+
+classes <- c("N", "p", "h", "V", "t", "i", "A", "v", "C", "P", "!", "r", "D", "I", "o")
+
+
+sapply(head(last), function(x){
+    grep(classes[1], x)
+})
+
+results <- t(sapply(last, function(x){
+                sapply(classes, function(y){
+                    ifelse(length(grep(y, x) > 1), 1, 0)
+                })
+            }))
+
+pos_final <- data.frame(word = first, results)
+rownames(pos_final) <- 1:nrow(pos_final)
+colnames(pos_final) <- c("word", "Noun", "Plural", "Noun_Phrase", "Verb_participle", "Verb_transitive",
+                                    "Verb_intransitive", "Adjective", "Adverb", "Conjunction", "Preposition",
+                                    "Interjection", "Pronoun", "Definite_Article", "Indefinite_Article", "Nominative")
+
+
+write.csv(pos_final, "data/pos.csv", row.names = FALSE)

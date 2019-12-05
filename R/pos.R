dc49ad2f6840c215a1d2ef8daceffb1c25e076d3
Matt Cook <cookm346@myumanitoba.ca>
2019-12-05 15:31:41 -0600

    add pos function

---
+++ R/pos.R
@@ -0,0 +1,17 @@
+#' pos
+#'
+#' Fetch parts of speech classes for a vector of strings
+#' @param x a vector of strings
+#' @return a dataframe of ratings
+#' @export
+
+pos <- function(x){
+    norms <- read.csv("data/pos.csv")
+
+    return(t(sapply(x, function(y){
+        y <- clean_text(y)
+        inds <- match(y, norms$word)
+        inds <- inds[!is.na(inds)]
+        colSums(norms[inds , -1])
+    })))
+}

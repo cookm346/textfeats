85a0e727e9c83f9557d646ee031f404ffc122970
Matt Cook <cookm346@myumanitoba.ca>
2020-02-06 13:15:19 -0600

    add semantics function

---
+++ R/semantics.R
@@ -0,0 +1,24 @@
+#' semantics
+#'
+#' Fetch semantic wor vectors for words or documents
+#' @param x a vector of strings
+#' @return a dataframe of semantic vectors
+#' @export
+
+semantics <- function(x, vectors){
+
+    out <- matrix(NA_real_, length(x), ncol(vectors))
+
+    for(i in seq_along(x)){
+        y <- clean_text(x)
+        print(y)
+        inds <- match(y, rownames(vectors))
+        inds <- inds[!is.na(inds)]
+        print(inds)
+        if(length(inds) > 1)
+            out[i, ] <- colMeans(vectors[inds , ])
+        else
+            out[i, ] <- vectors[inds , ]
+    }
+    return(out)
+}
